8ff86003e16c1ea031ca90e9fd489cdb1b972ea5
torihazi <lovebarusa19981225@icloud.com>
2024-07-02 13:29:16 +0900

    fin chapter 11

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {loading ? <h1>ロード中です</h1> : <h1>ポケモンデータを取得しました</h1>}
+    </div>
+  );
 }
 
 export default App;
